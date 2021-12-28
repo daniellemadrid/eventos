b1436554b194dc6a5168e4badbd0ec5fee68e8fc
danielle_madrid <daniellereismadridd@gmail.com>
2021-12-28 17:24:20 -0300

    contando clicks

--- src/components/contador.tsx
+++ src/components/contador.tsx
@@ -4,15 +4,19 @@ import { useState } from 'react';
 
 function Contador() {
     const [count, setCount] = useState(0);
+
+    const contando = () => {
+        setCount (count + 1);
+    };
   
     return (
       <div>
         {count}<br />
-        <button onClick={() => setCount(count + 1)}>
+        <button onClick={contando}>
           aumentando
         </button>
   
-      <button onClick={() => setCount(count - 1)}>
+      <button onClick={contando}>
         diminuindo
       </button>
     </div>
